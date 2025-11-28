65f21faf20510bc0a85920866af0452b6dde845f
Jacob Waldrip <jakeywaldrip@gmail.com>
2025-11-28 17:34:54 -0600

    Supporting mac

--- src/client.rs
+++ src/client.rs
@@ -9,6 +9,10 @@ use tar::Archive;
 use ureq::Error;
 use xz2::read::XzDecoder;
 
+// INFO:
+// Only the URL differs between these, but the implementation could differ significantly
+// if we decide to support windows, so leaving the implementation duplicated for now
+
 pub trait InstallStrategy {
     fn install(&self, version_num: &str) -> Result<(), Error>;
 }
@@ -65,10 +69,51 @@ impl InstallStrategy for LinuxClient {
 
 pub struct MacSiliconClient {}
 
-// TODO: impl mac install
 impl InstallStrategy for MacSiliconClient {
-    fn install(&self, _version_num: &str) -> Result<(), Error> {
-        println!("Mac install not yet implemented");
+    fn install(&self, version_num: &str) -> Result<(), Error> {
+        // Checking these in the parent, so they should never error
+        let rnvm_dir = env::var("RNVM_DIR").expect("RNVM_DIR not set");
+        let out_dir = PathBuf::from(rnvm_dir).join(version_num);
+        if Path::new(&out_dir).exists() {
+            println!("Node v{version_num} already installed");
+            return Ok(());
+        }
+
+        let spinner = ProgressBar::new_spinner();
+        spinner.set_style(
+            ProgressStyle::with_template("{spinner} Downloading from Nodejs...")
+                .unwrap()
+                .tick_strings(&["⠋", "⠙", "⠹", "⠸", "⠼", "⠴", "⠦", "⠧", "⠇", "⠏"]),
+        );
+        spinner.enable_steady_tick(Duration::from_millis(100));
+
+        let url = format!(
+            "https://nodejs.org/dist/v{version_num}/node-v{version_num}-darwin-arm64.tar.xz"
+        );
+        let response = ureq::get(&url).call();
+
+        let mut reader = match response {
+            Ok(resp) => resp.into_body().into_reader(),
+            Err(e) => return Err(e),
+        };
+
+        let tmp_dir = out_dir.with_extension("tmp");
+        fs::create_dir_all(&tmp_dir)?;
+
+        let decompressor = XzDecoder::new(&mut reader);
+        let mut archive = Archive::new(decompressor);
+        archive.unpack(&tmp_dir)?;
+
+        // Move the unzipped file into the output directory
+        let mut entries = fs::read_dir(&tmp_dir)?
+            .filter_map(Result::ok)
+            .filter(|e| e.file_type().map(|ft| ft.is_dir()).unwrap_or(false));
+
+        if let Some(first_dir) = entries.next() {
+            fs::rename(first_dir.path(), &out_dir)?;
+        }
+        fs::remove_dir_all(&tmp_dir)?;
+
         Ok(())
     }
 }
